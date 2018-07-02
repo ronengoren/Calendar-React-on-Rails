fe0aa7bbdde7e8af3ad5e866cceb919579eb4575
ronen.goren <ronen.goren@gmail.com>
2018-07-02 17:33:34 -0400

    added appoitment page

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,7 +1,7 @@
-var Appointments = React.createClass({
-    render: function() {
-     return (
-       <h1>CalReact</h1>
-     )
+class Appointments extends React.Component{
+    render() {
+        return(
+           <h1>CalReact</h1>
+        );
     }
-  })
+};
